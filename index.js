8fb86155beef3ade34a4ed3b054256ca21d61865
salmango <105051282+salmango@users.noreply.github.com>
2022-05-25 19:14:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// const express = require("express");
+// const app = express();
 
 app.get("/", function (req, res) {
   res.send("WORKING!!!");
@@ -9,3 +19,26 @@ app.listen(process.env.PORT || 8000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 //  app.listen(process.env.PORT || 8000);
+
+
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+
+// //create a server object:
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/view/hello.html");
+// });
+
+// app.use(express.static("src/view/"));
+
+// app.listen(8080, function() {
+//   console.log("server running on 8080");
+// }); //the server object listens on port 8080
+
+// module.exports = app;
