4f566a76d40aea05d72c2c00600b6bcee435df26
salmango <105051282+salmango@users.noreply.github.com>
2022-05-25 19:28:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.get("/", function (req, res) {
   res.send("WORKING!!!");
 });
 
-app.listen(process.env.PORT || 8000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
-//  app.listen(process.env.PORT || 8000);
+//  app.listen(process.env.PORT || 3000);
 
 
 // const express = require("express");
